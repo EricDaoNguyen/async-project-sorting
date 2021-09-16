5c3f3a0047a18072d5ad7dc6dc0c06d2a3f1876b
EricDaoNguyen <EricDaoNguyen@Gmail.com>
2021-09-16 16:44:21 -0400

    Added button to test and CSS styling.

--- src/SortingApp/SortingApp.jsx
+++ src/SortingApp/SortingApp.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Popup from 'reactjs-popup'
 import './style.css'
 import { mergeSort } from '../SortingAlgorithms/sortingAlgorithms'
 
@@ -26,11 +27,11 @@ function randomNumber(minimum, maximum) {
 // areEqual function
 // Checks to see if both arrays are the same length
 function areEqual(arrayOne, arrayTwo) {
-  if(arrayOne.length === arrayTwo.length) { return true }
+  if(arrayOne.length === arrayTwo.length) { return 'Your initial and post-sort results are the same!' }
   for(let i = 0; i < arrayOne.length; i++) {
-    if(arrayOne[i] === arrayTwo[i]) { return true }
+    if(arrayOne[i] === arrayTwo[i]) { return 'Your initial and post-sort results are the same!' }
   }
-  return false
+  return 'Your initial and post-sort results are NOT the same!'
 }
 
 // SortingApp component
@@ -52,17 +53,24 @@ export default class SortingApp extends React.Component {
     this.setState({ array })
   }
 
-  // testAlgorithms method
-
-  // mergeSort method
-  mergeSort() {
+  // testAlgorithm method
+  // Tests an algorithm 50 times to check if both the initial and post-sort arrays are the same
+  testAlgorithm() {
+    for(let i = 0; i < 50; i++) {
+      const array = []
+      const length = randomNumber(1, 1000)
+      for(let i = 0; i < length; i++) { array.push(randomNumber(-1000, 1000)) }
+    }
     const generatedArray = this.state.array
     .slice()
     .sort((a, b) => a - b)
     const sortedArray = mergeSort(this.state.array)
 
-    console.log(areEqual(generatedArray, sortedArray))
+    return window.alert(areEqual(generatedArray, sortedArray))
   }
+
+  // mergeSort method
+  // mergeSort() {}
 
   // bubbleSort method
 
@@ -74,9 +82,25 @@ export default class SortingApp extends React.Component {
       <div className="container">
 
         <div className="buttons">
-          <button className="button" onClick={() => this.resetArray()}>Generate New Bars</button>
-          <button className="button" onClick={() => this.mergeSort()}>Merge Sort</button>
-          <button className="button" onClick={() => this.bubbleSort()}>Bubble Sort</button>
+          <button className="buttonOne" onClick={() => this.resetArray()}>Generate New Bars</button>
+          <button className="buttonTwo" onClick={() => this.testAlgorithm()}>Test Algorithm</button>
+          <Popup
+          trigger={<div className="menu">Sorting Algorithms</div>}
+          position="right top"
+          on="hover"
+          closeOnDocumentClick
+          mouseLeaveDelay={100}
+          mouseEnterDelay={0}
+          contentStyle={{ padding: '0px', border: 'none' }}
+          arrow={false}
+          >
+            <div className="menu-list">
+              <button className="subButton" onClick={() => this.mergeSort()}>Merge Sort</button>
+              <button className="subButton" onClick={() => this.bubbleSort()}>Bubble Sort</button>
+            </div>
+          </Popup>
+
+
         </div>
 
         {array.map((value, index) => (
