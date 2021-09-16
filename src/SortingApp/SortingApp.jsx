324bb9f456f9991440914a4bb75869ed8bcef548
EricDaoNguyen <EricDaoNguyen@Gmail.com>
2021-09-16 13:31:30 -0400

    New buttons added, mergeSort currently logs true after comparing both initial and post-mergSort arrays.

--- src/SortingApp/SortingApp.jsx
+++ src/SortingApp/SortingApp.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './style.css'
+import { mergeSort } from '../SortingAlgorithms/sortingAlgorithms'
 
 /* ------------------------------------------------------------ */
 
@@ -16,9 +17,20 @@ import './style.css'
 
 /* ------------------------------------------------------------ */
 
+// randomNumber function
 // Generates randomly between two numbers
 function randomNumber(minimum, maximum) {
   return Math.floor(Math.random() * (maximum - minimum + 1) + minimum)
+}
+
+// areEqual function
+// Checks to see if both arrays are the same length
+function areEqual(arrayOne, arrayTwo) {
+  if(arrayOne.length === arrayTwo.length) { return true }
+  for(let i = 0; i < arrayOne.length; i++) {
+    if(arrayOne[i] === arrayTwo[i]) { return true }
+  }
+  return false
 }
 
 // SortingApp component
@@ -32,6 +44,7 @@ export default class SortingApp extends React.Component {
   // Every time the app loads, resetArray method is called
   componentDidMount() { this.resetArray() }
 
+  // resetArray method
   // Generates random bars
   resetArray() {
     const array = []
@@ -39,19 +52,41 @@ export default class SortingApp extends React.Component {
     this.setState({ array })
   }
 
+  // testAlgorithms method
+
+  // mergeSort method
+  mergeSort() {
+    const generatedArray = this.state.array
+    .slice()
+    .sort((a, b) => a - b)
+    const sortedArray = mergeSort(this.state.array)
+
+    console.log(areEqual(generatedArray, sortedArray))
+  }
+
+  // bubbleSort method
+
   // Renders from JSX to HTML
   render() {
     const { array } = this.state
 
     return(
       <div className="container">
-      {array.map((value, index) => (
-        <div
-        className="bar"
-        key={index}
-        style={{height:`${value}px`}}
-        ></div>
-      ))}
+
+        <div className="buttons">
+          <button className="button" onClick={() => this.resetArray()}>Generate New Bars</button>
+          <button className="button" onClick={() => this.mergeSort()}>Merge Sort</button>
+          <button className="button" onClick={() => this.bubbleSort()}>Bubble Sort</button>
+        </div>
+
+        {array.map((value, index) => (
+          <div
+          className="bar"
+          key={index}
+          style={{height:`${value}px`}}
+          ></div>
+        ))}
+
       </div>
     )
   }
